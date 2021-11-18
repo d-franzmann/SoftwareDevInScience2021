dd0e63c2ffacfab7f5a6a228497bd89c514c8e22
David Franzmann <d.franzmann@fz-juelich.de>
2021-11-18 13:01:09 +0100

    changes

--- math/simple_math.py
+++ math/simple_math.py
@@ -15,7 +15,9 @@ class testing(unittest.TestCase):
         assert result == 3
         
         self.assertRaises(TypeError, add, ('a', 2))
-        
+        self.assertRaises(TypeError, add, (2.1, 2))
+        self.assertRaises(TypeError, add, (2, 'a'))
+        self.assertRaises(TypeError, add, (2, 2.1))
 
 if __name__ == '__main__':
     unittest.main()
